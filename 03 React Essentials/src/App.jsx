77b6e53c9b9b63de130c9bc13045ef6cae614c4c
김상수 총괄 CONSULTANT/글로벌딜리버리추진팀 <sskim06@lgcns.com>
2024-07-18 09:33:05 +0900

    lecture 56

--- 03 React Essentials/src/App.jsx
+++ 03 React Essentials/src/App.jsx
@@ -1,16 +1,32 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  let tabContent = 'Please click a button';
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
-    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
   }
 
   console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -35,7 +51,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <p>{tabContent}</p>
+          {tabContent}
         </section>
       </main>
     </div>
